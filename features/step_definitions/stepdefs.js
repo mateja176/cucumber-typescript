f572421e9ee641134fd1c730e95fc6b47270e0be
Mateja Petrovic <mateja176@gmail.com>
2019-10-13 17:22:10 +0200

    See scenario reported as pending

--- features/step_definitions/stepdefs.js
+++ features/step_definitions/stepdefs.js
@@ -1,2 +1,17 @@
 const assert = require('assert');
 const { Given, When, Then } = require('cucumber');
+
+Given('today is Sunday', () => {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+When("I ask whether it's Friday yet", () => {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Then('I should be told {string}', string => {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
