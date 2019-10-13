483420c72e9f2d7c15fa32a3b6429dc9ed293e7a
Mateja Petrovic <mateja176@gmail.com>
2019-10-13 17:32:04 +0200

    See scenario reported as failing

--- features/step_definitions/stepdefs.js
+++ features/step_definitions/stepdefs.js
@@ -1,17 +1,18 @@
 const assert = require('assert');
 const { Given, When, Then } = require('cucumber');
 
-Given('today is Sunday', () => {
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending';
+function isItFriday(today) {
+  // We'll leave the implementation blank for now
+}
+
+Given('today is Sunday', function () {
+  this.today = 'Sunday';
 });
 
-When("I ask whether it's Friday yet", () => {
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending';
+When("I ask whether it's Friday yet", function () {
+  this.actualAnswer = isItFriday(this.today);
 });
 
-Then('I should be told {string}', string => {
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending';
+Then('I should be told {string}', function (expectedAnswer) {
+  assert.equal(this.actualAnswer, expectedAnswer);
 });
